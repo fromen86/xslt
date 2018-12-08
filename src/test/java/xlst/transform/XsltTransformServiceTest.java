915d1d1e71e4ea61270fcb53ac7a304185ad4a42
Akhramovich <makhramovich@alvioneurope.ru>
2018-12-08 14:15:27 +0300

    Use more correct XML comparison

--- src/test/java/xlst/transform/XsltTransformServiceTest.java
+++ src/test/java/xlst/transform/XsltTransformServiceTest.java
@@ -1,19 +1,23 @@
 package xlst.transform;
 
 import org.apache.commons.io.FileUtils;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.xmlunit.diff.Diff;
 import xlst.XsltTestConfiguration;
 import xslt.service.XsltTransformService;
 
 import javax.xml.transform.TransformerException;
+import javax.xml.transform.stream.StreamSource;
 import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
+import java.util.Collections;
 
 /**
  * @author makhramovich
@@ -30,7 +34,11 @@ public class XsltTransformServiceTest {
     String inputXml = FileUtils.readFileToString(new File(getPath("/transform/original_order.xml")), "UTF-8");
     String outputXml = transformService.transform(inputXml);
     String expectedOutputXml = FileUtils.readFileToString(new File(getPath("/transform/converted.xml")), "UTF-8");
-    assertEquals(expectedOutputXml, outputXml);
+    assertFalse(new Diff(
+            new StreamSource(new StringReader(expectedOutputXml)),
+            new StreamSource(new StringReader(outputXml)),
+            Collections.emptyList()
+    ).hasDifferences());
   }
 
   private String getPath(String localPath) {
